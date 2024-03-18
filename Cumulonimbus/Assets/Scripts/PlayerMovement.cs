11821e93d802ab0350dd77277e62682ee4923310
Timlour <timlour16@gmail.com>
2024-03-17 23:05:05 -0400

    For Project 1 submission

--- Cumulonimbus/Assets/Scripts/PlayerMovement.cs
+++ Cumulonimbus/Assets/Scripts/PlayerMovement.cs
@@ -6,27 +6,31 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float vertical;
     public float speed = 8f;
     public float jumpingPower = 16f;
     private bool isFacingRight = true;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    private bool isCloseToLadder = false;
-    private bool climbHeld = false;
-    private bool hasStartedClimb = false;
+    private bool isCloseToLadder = false; // set true when player is near ladder but has not started climbing yet
+    private bool climbHeld = false; // set true when player presses and holds climb button
+    private bool hasStartedClimb = false; // control bool used to prevent other player actions while climbing
+    private Transform ladder;
+    private float vertical;
+    private float climbSpeed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.instance.Play("WorldOfPain");
+        AudioManager.instance.Play("RainThunder");
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        
+        vertical = Input.GetAxisRaw("Vertical") * climbSpeed;
+
         if (Input.GetKeyDown(KeyCode.A) && IsGrounded()) // Press A to jump
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
@@ -39,11 +43,46 @@ public class PlayerMovement : MonoBehaviour
 
         Flip();
 
+        climbHeld = (isCloseToLadder && Input.GetKey(KeyCode.DownArrow)) ? true : false;
+
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // Climbing
+        if (hasStartedClimb && !climbHeld)
+        {
+            if (horizontal > 0 || horizontal < 0) ResetClimbing();
+        }
+        else if (hasStartedClimb && climbHeld)
+        {
+            float height = GetComponent<SpriteRenderer>().size.y;
+            float topHandlerY = Half(ladder.transform.GetChild(0).transform.position.y + height);
+            float bottomHandlerY = Half(ladder.transform.GetChild(1).transform.position.y + height);
+            float transformY = Half(transform.position.y);
+            float transformVY = transformY + vertical;
+
+            if (transformVY > topHandlerY || transformVY < bottomHandlerY)
+            {
+                ResetClimbing();
+            }
+            else if (transformY <= topHandlerY && transformY >= bottomHandlerY)
+            {
+                rb.bodyType = RigidbodyType2D.Kinematic;
+                if (!transform.position.x.Equals(ladder.transform.position.x))
+                    transform.position = new Vector3(ladder.transform.position.x, transform.position.y, transform.position.z);
+
+                Vector3 forwardDirection = new Vector3(0, transformVY, 0);
+                Vector3 newPos = Vector3.zero;
+                if (vertical > 0)
+                    newPos = transform.position + forwardDirection * Time.deltaTime * climbSpeed;
+                else if (vertical < 0)
+                    newPos = transform.position - forwardDirection * Time.deltaTime * climbSpeed;
+                if (newPos != Vector3.zero) rb.MovePosition(newPos);
+            }
+        }
     }
 
     private bool IsGrounded()
@@ -61,5 +100,32 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+ 
+        if (collision.gameObject.tag.Equals("Ladder"))
+        {
+            isCloseToLadder = true;
+            this.ladder = collision.transform;
+        }
+    }
+
+    public static float Half(float value)
+    {
+        return Mathf.Floor(value) + 0.5f;
+    }
+
+    private void ResetClimbing()
+    {
+        if (hasStartedClimb)
+        {
+            hasStartedClimb = false;
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            transform.position = new Vector3(transform.position.x, Half(transform.position.y), transform.position.z);
+        }
+    }
+
+
 }
 
